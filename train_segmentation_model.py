6ad72213513803b402b86da4bd787a79c585d6e3
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-06-11 13:26:04 +0100

    ...

--- train_segmentation_model.py
+++ train_segmentation_model.py
@@ -5,7 +5,8 @@ from torch.optim import lr_scheduler
 import torch.backends.cudnn as cudnn
 import numpy as np
 import torchvision
-from torchvision import datasets, models, transforms
+from torchvision import datasets, transforms
+import torchvision.models.segmentation as models
 import matplotlib.pyplot as plt
 import time
 from datetime import datetime
