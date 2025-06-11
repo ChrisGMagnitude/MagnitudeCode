53b1b900385aca17c328427aca02821480c0dd35
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-06-11 17:10:24 +0100

    ...

--- train.py
+++ train.py
@@ -75,8 +75,8 @@ def train_model(model, criterion, optimizer, scheduler, device, dataloaders, log
                     
                     running_IOU.append(miou(torch.round(preds).int(), labels.int()).cpu())
                     print(running_IOU)
-                    print(max(torch.round(preds).int(),max(labels.int())))
-                    print(min(torch.round(preds).int(),min(labels.int())))
+                    print(max(torch.round(preds).int()),max(labels.int()))
+                    print(min(torch.round(preds).int()),min(labels.int()))
 
                 if phase == 'train':
                     scheduler.step()
