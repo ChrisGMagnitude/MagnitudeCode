f8f7385c05da171640377639f558e582dfe8ef39
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-06-12 14:45:35 +0100

    ...

--- train_segmentation_model.py
+++ train_segmentation_model.py
@@ -29,11 +29,11 @@ num_workers = 8#40
 description = 'dice-loss'
 trainging_mode = 'head'#'all'#'head'#'final-fc'#'first-conv'
 initial_weights = r'/mnt/magbucket/segmentation/Models/dice-loss - head - 2025-06-12 102442'#'default'#
-lr = 0.02#0.0005#0.02#0.1
+lr = 0.1#0.0005#0.02#0.1
 momentum = 0.9
 step_size = 10
 gamma = 0.5 # 0.6
-num_epochs = 10
+num_epochs = 5
 
 
 model_path = r'/mnt/magbucket/segmentation/Models'
