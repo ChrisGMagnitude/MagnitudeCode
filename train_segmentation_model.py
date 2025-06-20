8bc579abe1d10b44ae311d86e3e3d3e8b6fce5b9
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-06-20 10:50:51 +0100

    ...

--- train_segmentation_model.py
+++ train_segmentation_model.py
@@ -38,7 +38,7 @@ num_epochs = 5
 
 model_path = r'/mnt/magbucket/segmentation/Models'
 
-train_dataset = MagClassDataset(r'/mnt/magbucket/segmentation/train.hdf5',augment=False,label_type=label_type)
+train_dataset = MagClassDataset(r'/mnt/magbucket/segmentation/train.hdf5',augment=True,label_type=label_type)
 val_dataset = MagClassDataset(r'/mnt/magbucket/segmentation/valid.hdf5',augment=False,label_type=label_type)
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, pin_memory=True,num_workers=num_workers,shuffle=True)  
