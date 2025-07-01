5300dfa9ccff1c8774fb95cf2fc4351d1942409c
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-07-01 15:09:19 +0100

    head

--- train_segmentation_model.py
+++ train_segmentation_model.py
@@ -27,13 +27,13 @@ epoch_size_val = 20*32*5#1280
 batch_size = 40#32
 num_workers = 8#40
 description = 'balanced'
-trainging_mode = 'final-fc'#'all'#'head'#'final-fc'#'first-conv'
-initial_weights = 'default'#r'/mnt/magbucket/segmentation/Models/balanced - final-fc - 2025-06-25 093516'#'default'#
-lr = 0.1#0.0005#0.02#0.1
+trainging_mode = 'head'#'all'#'head'#'final-fc'#'first-conv'
+initial_weights = r'/mnt/magbucket/segmentation/Models/balanced - final-fc - 2025-07-01 131705'#'default'#
+lr = 0.02#0.0005#0.02#0.1
 momentum = 0.9
 step_size = 10
 gamma = 0.25 # 0.6
-num_epochs = 5
+num_epochs = 15
 interp_id_lookup = {}
 interp_id_lookup["combinedMask"] = ['Agricultural (Strong)Mask',
                                     'Agricultural (Weak)Mask',
