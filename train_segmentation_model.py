3a6c558729b3b9cc9801ac2ee4c2378f0f480815
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-07-02 11:29:43 +0100

    ...

--- train_segmentation_model.py
+++ train_segmentation_model.py
@@ -133,13 +133,13 @@ if trainging_mode=='head':
     optimizer_ft = optim.SGD(model.classifier.parameters(), lr=lr, momentum=momentum,weight_decay=weight_decay)
 elif trainging_mode=='final-fc':
     print('final-fc')
-    optimizer_ft = optim.SGD(model.classifier[4].parameters(), lr=lr, momentum=momentum,weight_decay=weight_decay))
+    optimizer_ft = optim.SGD(model.classifier[4].parameters(), lr=lr, momentum=momentum,weight_decay=weight_decay)
 elif trainging_mode=='all':
     print('all')
-    optimizer_ft = optim.SGD(model.parameters(), lr=lr, momentum=momentum,weight_decay=weight_decay))
+    optimizer_ft = optim.SGD(model.parameters(), lr=lr, momentum=momentum,weight_decay=weight_decay)
 elif trainging_mode=='first-conv':
     print('first-conv')
-    optimizer_ft = optim.SGD(model.features[0].parameters(), lr=lr, momentum=momentum,weight_decay=weight_decay))   
+    optimizer_ft = optim.SGD(model.features[0].parameters(), lr=lr, momentum=momentum,weight_decay=weight_decay)
     
 # Decay LR by a factor of gamma every step_size epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=step_size, gamma=gamma)
