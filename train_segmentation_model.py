258e573ccee35e7da855e00a512022931e273f17
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-07-03 16:22:34 +0100

    ...

--- train_segmentation_model.py
+++ train_segmentation_model.py
@@ -108,9 +108,12 @@ num_classes = len(train_dataset.label_fields)
 #model = models.deeplabv3_resnet50(pretrained=True)
 #model.classifier[4] = torch.nn.Conv2d(256, num_classes, kernel_size=(1, 1), stride=(1, 1))
 
-model = models.lraspp_mobilenet_v3_large(pretrained=True)
-model.classifier.low_classifier = torch.nn.Conv2d(40, num_classes, kernel_size=(1, 1), stride=(1, 1))
-model.classifier.high_classifier = torch.nn.Conv2d(128, num_classes, kernel_size=(1, 1), stride=(1, 1))
+#model = models.lraspp_mobilenet_v3_large(pretrained=True)
+#model.classifier.low_classifier = torch.nn.Conv2d(40, num_classes, kernel_size=(1, 1), stride=(1, 1))
+#model.classifier.high_classifier = torch.nn.Conv2d(128, num_classes, kernel_size=(1, 1), stride=(1, 1))
+
+model = models.fcn_resnet50
+print(model)
 
 
 if initial_weights != 'default':
