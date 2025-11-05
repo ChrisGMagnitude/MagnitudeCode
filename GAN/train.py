d8e4cd9172e5bb38a2e1a768d1b297233683c073
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-11-05 12:51:13 +0000

    ....

--- GAN/train.py
+++ GAN/train.py
@@ -180,18 +180,18 @@ def train_model(model, netD, optimizerG, optimizerD, criterion,
             if phase == 'train':
                 #scheduler.step()
                 if len(train_loss_g)>0:
-                    train_loss_g_epoch.append(np.mean(train_loss_g))
+                    train_loss_g_epoch.append(str(np.mean(train_loss_g)))
                     print(f'{phase} Model Loss: {np.mean(train_loss_g):.4f}')
                 if len(train_loss_d)>0:
-                    train_loss_d_epoch.append(np.mean(train_loss_d))
+                    train_loss_d_epoch.append(str(np.mean(train_loss_d)))
                     print(f'{phase} Discriminator Loss: {np.mean(train_loss_d):.4f}')
                 
             else:
                 if len(val_loss_g)>0:
-                    val_loss_g_epoch.append(np.mean(val_loss_g))
+                    val_loss_g_epoch.append(str(np.mean(val_loss_g)))
                     print(f'{phase} Model Loss: {np.mean(val_loss_g):.4f}')
                 if len(val_loss_d)>0:
-                    val_loss_d_epoch.append(np.mean(val_loss_d))
+                    val_loss_d_epoch.append(str(np.mean(val_loss_d)))
                     print(f'{phase} Discriminator Loss: {np.mean(val_loss_d):.4f}')
                 
             
