6f7d922510dcdd15ee01bd4e968d3c45aafcaf69
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-11-14 13:25:43 +0000

    max pool instead of linear

--- GAN/run_GAN_training.py
+++ GAN/run_GAN_training.py
@@ -137,26 +137,28 @@ class Discriminator(nn.Module):
             # input is ``(nc) x 64 x 64``
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
+            
             # state size. ``(ndf) x 32 x 32``
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            #nn.BatchNorm2d(ndf * 2, track_running_stats=False),
-            nn.Dropout(p=0.2),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.BatchNorm2d(ndf * 2, track_running_stats=False),
             nn.LeakyReLU(0.2, inplace=True),
+            
             # state size. ``(ndf*2) x 16 x 16``
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            #nn.BatchNorm2d(ndf * 4, track_running_stats=False),
-            nn.Dropout(p=0.2),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.BatchNorm2d(ndf * 4, track_running_stats=False),
             nn.LeakyReLU(0.2, inplace=True),
+            
             # state size. ``(ndf*4) x 8 x 8``
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            #nn.BatchNorm2d(ndf * 8, track_running_stats=False),
-            nn.Dropout(p=0.2),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.BatchNorm2d(ndf * 8, track_running_stats=False),
             nn.LeakyReLU(0.2, inplace=True),
+            
             # state size. ``(ndf*8) x 4 x 4``
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-            nn.Flatten(),
-            nn.Linear(23*23, 1),
-            #nn.Sigmoid()
+            nn.Conv2d(ndf * 8, 1, 3, 1, 0, bias=False),
+
         )
 
     def forward(self, input):
